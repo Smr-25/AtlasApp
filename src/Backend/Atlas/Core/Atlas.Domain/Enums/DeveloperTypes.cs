1127f08b896915fd7ff5de5d96ec1cd171bbf4e6
Smr-25 <samiraa@code.edu.az>
2026-02-11 19:54:58 +0400

    11.02.26 final project v2

---
+++ src/Backend/Atlas/Core/Atlas.Domain/Enums/DeveloperTypes.cs
@@ -0,0 +1,13 @@
+namespace Atlas.Domain.Enums;
+
+public enum DeveloperTypes
+{
+    Frontend = 1,
+    Backend = 2,
+    FullStack = 3,
+    Mobile = 4,
+    DevOps = 5,
+    DataScience = 6,
+    GameDevelopment = 7,
+    EmbeddedSystems = 8
+}
